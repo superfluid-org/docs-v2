23684a9dea0ba01a24ad30d9a7476689f4bf4c1a
youssefea <youcefea99@gmail.com>
2023-11-30 23:02:11 +0000

    adding example

--- src/components/Visualizations/GdaSuperExampleVis/index.tsx
+++ src/components/Visualizations/GdaSuperExampleVis/index.tsx
@@ -2,113 +2,137 @@ import React, { useEffect, useRef } from 'react';
 import * as d3 from 'd3';
 
 const SuperfluidPoolVisualization = ({ width = 800, height = 600 }) => {
-    const ref = useRef();
+  const ref = useRef();
 
-    useEffect(() => {
-        const svg = d3.select(ref.current)
-        .attr('width', width)
-        .attr('height', height);
-  
-      const poolCenter = { x: width / 2, y: height / 2 };
-      const poolRadius = 50;
-      const memberCount = 5;
-      const memberRadius = 15;
-      const streamPathLength = 200;
-      const unitFlowSpeed = 2000; // Speed of unit flow
-      const higherStreamerPosition = { x: poolCenter.x - 250, y: poolCenter.y - 100 }; // Position of the higher streamer
-      const adminPosition = { x: poolCenter.x - 250, y: poolCenter.y + 100 }; // Adjusted position of the pool admin
-  
-      // Create pool with label
-      svg.append('circle')
-        .attr('cx', poolCenter.x)
-        .attr('cy', poolCenter.y)
-        .attr('r', poolRadius)
-        .attr('fill', 'green');
-  
-      svg.append('text')
-        .attr('x', poolCenter.x)
-        .attr('y', poolCenter.y)
-        .attr('text-anchor', 'middle')
-        .attr('dy', '0.3em')
-        .text('Distribution Pool')
-        .style('fill', "white");
-  
-      // Create higher streamer with label
-      svg.append('circle')
-        .attr('cx', higherStreamerPosition.x)
-        .attr('cy', higherStreamerPosition.y)
-        .attr('r', memberRadius)
-        .attr('fill', 'red');
-  
-      svg.append('text')
-        .attr('x', higherStreamerPosition.x)
-        .attr('y', higherStreamerPosition.y - memberRadius - 5)
-        .attr('text-anchor', 'middle')
-        .text('Higher Streamer')
-        .style('fill', "white");
-  
-      // Create pool admin with label
-      svg.append('circle')
-        .attr('cx', adminPosition.x)
-        .attr('cy', adminPosition.y)
-        .attr('r', memberRadius)
-        .attr('fill', 'blue');
-  
-      svg.append('text')
-        .attr('x', adminPosition.x)
-        .attr('y', adminPosition.y - memberRadius - 5)
-        .attr('text-anchor', 'middle')
-        .text('Pool Admin')
-        .style('fill', "white");
-  
-      // Function to create moving units
-      const createMovingUnits = (path, color, speedMultiplier) => {
-        for (let j = 0; j < 20; j++) {
-          svg.append('circle')
-            .attr('r', 5)
-            .attr('fill', color)
-            .transition()
-            .duration(unitFlowSpeed / speedMultiplier)
-            .ease(d3.easeLinear)
-            .attrTween('transform', () => {
-              return (t) => {
-                const p = path.node().getPointAtLength(t * path.node().getTotalLength());
-                return `translate(${p.x}, ${p.y})`;
-              };
-            })
-            .on('end', function repeat() {
-              d3.select(this)
-                .transition()
-                .duration(unitFlowSpeed / speedMultiplier)
-                .ease(d3.easeLinear)
-                .attrTween('transform', () => {
-                  return (t) => {
-                    const p = path.node().getPointAtLength(t * path.node().getTotalLength());
-                    return `translate(${p.x}, ${p.y})`;
-                  };
-                })
-                .on('end', repeat);
-            });
-        }
-      };
-  
-      // Create stream from higher streamer to pool
-      const higherStreamerPath = svg.append('path')
-        .attr('d', `M ${higherStreamerPosition.x} ${higherStreamerPosition.y} L ${poolCenter.x} ${poolCenter.y}`)
-        .attr('id', 'higher-streamer-path')
-        .attr('fill', 'none')
-        .attr('stroke', 'none');
-  
-      createMovingUnits(higherStreamerPath, 'red', 0.5); // Higher flow rate
-  
-      // Create stream from admin to pool
-      const adminPath = svg.append('path')
-        .attr('d', `M ${adminPosition.x} ${adminPosition.y} L ${poolCenter.x} ${poolCenter.y}`)
-        .attr('id', 'admin-path')
-        .attr('fill', 'none')
-        .attr('stroke', 'none');
-  
-      createMovingUnits(adminPath, 'gold', 1); // Normal flow rate
+  useEffect(() => {
+    const svg = d3.select(ref.current)
+      .attr('width', width)
+      .attr('height', height);
+
+    const poolCenter = { x: width / 2, y: height / 2 };
+    const poolRadius = 50;
+    const memberCount = 5;
+    const memberRadius = 15;
+    const streamPathLength = 200;
+    const unitFlowSpeed = 2000; // Speed of unit flow
+    const higherStreamerPosition = { x: poolCenter.x - 250, y: poolCenter.y - 100 }; // Position of the higher streamer
+    const adminPosition = { x: poolCenter.x - 250, y: poolCenter.y + 100 }; // Adjusted position of the pool admin
+
+    // Create pool with label
+    svg.append('circle')
+      .attr('cx', poolCenter.x)
+      .attr('cy', poolCenter.y)
+      .attr('r', poolRadius)
+      .attr('fill', 'green');
+
+    svg.append('text')
+      .attr('x', poolCenter.x)
+      .attr('y', poolCenter.y)
+      .attr('text-anchor', 'middle')
+      .attr('dy', '0.3em')
+      .text('Distribution Pool')
+      .style('fill', "white");
+
+    // Create higher streamer with label
+    svg.append('circle')
+      .attr('cx', higherStreamerPosition.x)
+      .attr('cy', higherStreamerPosition.y)
+      .attr('r', memberRadius)
+      .attr('fill', 'red');
+
+    svg.append('text')
+      .attr('x', higherStreamerPosition.x)
+      .attr('y', higherStreamerPosition.y - memberRadius - 5)
+      .attr('text-anchor', 'middle')
+      .text('Higher Streamer')
+      .style('fill', "white");
+
+    // Pool Admin initially as blue, later becomes a member
+    const poolAdmin = svg.append('circle')
+      .attr('cx', adminPosition.x)
+      .attr('cy', adminPosition.y)
+      .attr('r', memberRadius)
+      .attr('fill', 'blue');
+
+    const poolAdminLabel = svg.append('text')
+      .attr('x', adminPosition.x)
+      .attr('y', adminPosition.y - memberRadius - 5)
+      .attr('text-anchor', 'middle')
+      .text('Initial Streamer/Pool Admin')
+      .style('fill', "white");
+
+    // Function to create moving units
+    const createMovingUnits = (path, color, speedMultiplier, unitClass) => {
+      for (let j = 0; j < 20; j++) {
+        svg.append('circle')
+          .attr('class', unitClass) // Add a class to the units for easy selection and removal
+          .attr('r', 5)
+          .attr('fill', color)
+          .transition()
+          .duration(unitFlowSpeed / speedMultiplier)
+          .ease(d3.easeLinear)
+          .attrTween('transform', () => {
+            return (t) => {
+              const p = path.node().getPointAtLength(t * path.node().getTotalLength());
+              return `translate(${p.x}, ${p.y})`;
+            };
+          })
+          .on('end', function repeat() {
+            d3.select(this)
+              .transition()
+              .duration(unitFlowSpeed / speedMultiplier)
+              .ease(d3.easeLinear)
+              .attrTween('transform', () => {
+                return (t) => {
+                  const p = path.node().getPointAtLength(t * path.node().getTotalLength());
+                  return `translate(${p.x}, ${p.y})`;
+                };
+              })
+              .on('end', repeat);
+          });
+      }
+    };
+
+    // Function to start the animation
+    const startAnimation = () => {
+      changeAdminToMember();
+      createMovingUnits(higherStreamerPath, 'red', 0.5, 'higher-streamer-unit');
+    };
+
+    // Function to reset the animation
+    const resetAnimation = () => {
+      poolAdmin.transition().duration(1000).attr('fill', 'blue');
+      poolAdminLabel.text('Previous Streamer');
+      svg.selectAll('.higher-streamer-unit, .admin-unit').remove();
+    };
+
+
+    // Change Pool Admin to a member
+    const changeAdminToMember = () => {
+      poolAdmin.transition()
+        .duration(1000)
+        .attr('fill', 'orange');
+      poolAdminLabel.text('Member F');
+      createMovingUnits(adminPath, 'gold', 0.2); // Start streaming to the new member
+    };
+
+    // Create stream from higher streamer to pool
+    const higherStreamerPath = svg.append('path')
+      .attr('d', `M ${higherStreamerPosition.x} ${higherStreamerPosition.y} L ${poolCenter.x} ${poolCenter.y}`)
+      .attr('id', 'higher-streamer-path')
+      .attr('fill', 'none')
+      .attr('stroke', 'none');
+
+    createMovingUnits(higherStreamerPath, 'red', 5);
+
+    // Create stream path from pool to admin (reversed direction)
+    const adminPath = svg.append('path')
+      .attr('d', `M ${poolCenter.x} ${poolCenter.y} L ${adminPosition.x} ${adminPosition.y}`)
+      .attr('id', 'admin-path')
+      .attr('fill', 'none')
+      .attr('stroke', 'none');
+
+    setTimeout(() => changeAdminToMember(), 5000); 
 
     // Generate members and their paths
     const angleStep = (2 * Math.PI) / memberCount;
@@ -168,6 +192,16 @@ const SuperfluidPoolVisualization = ({ width = 800, height = 600 }) => {
           });
       }
     }
+
+    startAnimation();
+
+    // Interval to repeat the animation
+    const animationDuration = 10000; // Duration for one cycle of animation
+    setInterval(() => {
+      resetAnimation();
+      // Restart animation after a short delay
+      setTimeout(startAnimation, 1000); 
+    }, animationDuration);
   }, []);
 
   return <svg ref={ref}></svg>;
