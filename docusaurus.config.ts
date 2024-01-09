53b08c5440204b6b272bcac0d354267f86999650
youssefea <youcefea99@gmail.com>
2024-01-09 13:08:14 +0000

    change url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -9,7 +9,7 @@ const config: Config = {
   themes: ['@docusaurus/theme-live-codeblock'],
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://v2.docs.superfluid.finance/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
