d55d0cc29c3b13ef2a9a97c4d775f6f01ce2aa85
youssefea <youcefea99@gmail.com>
2024-02-11 15:35:30 +0000

    new redirect

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -251,30 +251,12 @@ const config: Config = {
       [
         '@docusaurus/plugin-client-redirects',
         {
-          fromExtensions: ['html', 'htm'], // /myPage.html -> /myPage
-          toExtensions: ['exe', 'zip'], // /myAsset -> /myAsset.zip (if latter exists)
           redirects: [
-            // /docs/oldDoc -> /docs/newDoc
             {
-              to: '/docs/newDoc',
-              from: '/docs/oldDoc',
-            },
-            // Redirect from multiple old paths to the new path
-            {
-              to: '/docs/newDoc2',
-              from: ['/docs/oldDocFrom2019', '/docs/legacyDocFrom2016'],
+              to: '/docs/concepts/superfluid',
+              from: '/superfluid/protocol-overview/in-depth-overview',
             },
           ],
-          createRedirects(existingPath) {
-            if (existingPath.includes('/community')) {
-              // Redirect from /docs/team/X to /community/X and /docs/support/X to /community/X
-              return [
-                existingPath.replace('/community', '/docs/team'),
-                existingPath.replace('/community', '/docs/support'),
-              ];
-            }
-            return undefined; // Return a falsy value: no redirect created
-          },
         },
       ],
     ],
