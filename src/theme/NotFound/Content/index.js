1476508a0ef006018feab48d57096a673669248a
youssefea <youcefea99@gmail.com>
2024-02-14 11:50:38 +0000

    add disclaimer to 404 page

---
+++ src/theme/NotFound/Content/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import clsx from 'clsx';
+import Translate from '@docusaurus/Translate';
+import Heading from '@theme/Heading';
+export default function NotFoundContent({className}) {
+  return (
+    <main className={clsx('container margin-vert--xl', className)}>
+      <div className="row">
+        <div className="col col--6 col--offset-3">
+          <Heading as="h1" className="hero__title">
+            <Translate
+              id="theme.NotFound.title"
+              description="The title of the 404 page">
+              Page Not Found
+            </Translate>
+          </Heading>
+          <p>
+            <Translate
+              id="theme.NotFound.p1"
+              description="The first paragraph of the 404 page">
+              We just transitioned to a new version of our technical documentation.
+            </Translate>
+          </p>
+          <p>
+            <Translate
+              id="theme.NotFound.p2"
+              description="The 2nd paragraph of the 404 page">
+              If you cannot find the content you were looking for,
+              please use the search function or check out our Legacy Docs: https://superfluid.gitbook.io .
+              Thank you for understanding!
+            </Translate>
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+}
