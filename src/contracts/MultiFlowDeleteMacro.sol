6541971877d29f60a07f845475f22e318219ed6c
youssefea <youcefea99@gmail.com>
2024-02-28 17:49:39 +0000

    adding batch transactions page and change order

---
+++ src/contracts/MultiFlowDeleteMacro.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: AGPLv3
+pragma solidity 0.8.23;
+
+import { ISuperfluid, BatchOperation, IConstantFlowAgreementV1, ISuperToken }
+    from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
+import { IUserDefinedMacro } from "@superfluid-finance/ethereum-contracts/contracts/interfaces/utils/IUserDefinedMacro.sol";
+
+
+// deletes a bunch of flows of the msgSender
+contract MultiFlowDeleteMacro is IUserDefinedMacro {
+    function buildBatchOperations(ISuperfluid host, bytes memory params, address msgSender) public virtual view
+        returns (ISuperfluid.Operation[] memory operations)
+    {
+        IConstantFlowAgreementV1 cfa = IConstantFlowAgreementV1(address(host.getAgreementClass(
+            keccak256("org.superfluid-finance.agreements.ConstantFlowAgreement.v1")
+        )));
+
+        // parse params
+        (ISuperToken token, address[] memory receivers) =
+            abi.decode(params, (ISuperToken, address[]));
+
+        // construct batch operations
+        operations = new ISuperfluid.Operation[](receivers.length);
+        for (uint i = 0; i < receivers.length; ++i) {
+            bytes memory callData = abi.encodeCall(cfa.deleteFlow,
+                                                   (token,
+                                                    msgSender,
+                                                    receivers[i],
+                                                    new bytes(0) // placeholder
+                                                   ));
+            operations[i] = ISuperfluid.Operation({
+                operationType : BatchOperation.OPERATION_TYPE_SUPERFLUID_CALL_AGREEMENT, // type
+                target: address(cfa),
+                data: abi.encode(callData, new bytes(0))
+            });
+        }
+    }
+
+    // returns the abi encoded params for the macro, to be used with buildBatchOperations
+    function getParams(ISuperToken superToken, address[] memory receivers) external pure returns (bytes memory) {
+        return abi.encode(superToken, receivers);
+    }
+}
