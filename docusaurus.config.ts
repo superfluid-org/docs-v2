beda83a57b1233f4351b4ee52c17bb50402683bb
youssefea <youcefea99@gmail.com>
2024-03-01 10:56:08 +0000

    add headtags

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -65,6 +65,26 @@ const config: Config = {
       disableSwitch: false,
       respectPrefersColorScheme: true,
     },
+    headTags: [
+      {
+        tagName: 'script',
+        innerHTML: `
+          var _paq = window._paq = window._paq || [];
+          _paq.push(['trackPageView']);
+          _paq.push(['enableLinkTracking']);
+          (function() {
+            var u="https://docsv2superfluidfinancevercelapp.matomo.cloud/";
+            _paq.push(['setTrackerUrl', u+'matomo.php']);
+            _paq.push(['setSiteId', '1']);
+            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+            g.async=true; g.src='https://cdn.matomo.cloud/docsv2superfluidfinancevercelapp.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+          })();
+        `,
+        attributes: {
+          async: true,
+        },
+      },
+    ],
     image: "img/superfluid-social-card.png",
     navbar: {
       title: "Docs",
